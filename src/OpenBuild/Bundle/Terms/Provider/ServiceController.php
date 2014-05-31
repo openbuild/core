6636507960344d689a39c7bef93cab7c5925060e
dannylewis <dannylewis.sheffield@googlemail.com>
2014-05-31 21:28:03 +0100

    Added start for a service controller for 'Terms'

---
+++ src/OpenBuild/Bundle/Terms/Provider/ServiceController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace OpenBuild\Bundle\Terms\Provider;
+
+use OpenBuild\Abstracts\ServiceController AS AbstractServiceController;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ServiceController extends AbstractServiceController
+{
+
+	//Contoller interface
+	public function connect(Application $app)
+	{
+	
+		// creates a new controller based on the default route
+		$controllers = $app['controllers_factory'];
+
+		$controllers->get('/index.html', function (Application $app){
+				
+			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/terms/index.html';
+			$appFile = $app['spa_files_dir'] . 'terms/index.html';
+			
+			if(file_exists($localFile)){
+			
+				return new Response(
+            		file_get_contents($localFile),
+					200
+				);
+				
+			}elseif(file_exists($appFile)){
+					
+				return new Response(
+            		file_get_contents($appFile),
+					200
+				);
+					
+			}else{
+				
+				$app->abort(404, "Could not find view file index.html");
+				
+			}
+			
+		});
+
+		$controllers->get('/cookies.html', function (Application $app){
+				
+			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/terms/cookies.html';
+			$appFile = $app['spa_files_dir'] . 'terms/cookies.html';
+			
+			if(file_exists($localFile)){
+			
+				return new Response(
+            		file_get_contents($localFile),
+					200
+				);
+				
+			}elseif(file_exists($appFile)){
+					
+				return new Response(
+            		file_get_contents($appFile),
+					200
+				);
+					
+			}else{
+				
+				$app->abort(404, "Could not find view file index.html");
+				
+			}
+			
+		});
+
+		$controllers->get('/index.js', function (Application $app){
+		
+			$appFile = $app['spa_files_dir'] . 'terms/index.js';
+			
+			if(file_exists($appFile)){
+					
+				return new Response(
+            		file_get_contents($appFile),
+					200,
+					array('content-type' => 'application/javascript')
+				);
+					
+			}else{
+				
+				$app->abort(404, "Could not find view file index.js");
+				
+			}
+			
+		});
+
+		return $controllers;
+
+	}
+
+	//Service interface
+	public function register(Application $app)
+	{
+        
+    }
+
+	//Service interface
+    public function boot(Application $app)
+    {
+
+		
+    	
+    }
+
+}
