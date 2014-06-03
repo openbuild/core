c20faabea633f7fc0b2f13a1f7e4e42c9640236b
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-03 21:12:40 +0100

    Moved to service interface

--- src/OpenBuild/Bundle/Flickr/Provider/ServiceController.php
+++ src/OpenBuild/Bundle/Flickr/Provider/ServiceController.php
@@ -14,63 +14,29 @@ class ServiceController extends AbstractServiceController
 	//Contoller interface
 	public function connect(Application $app)
 	{
-	
+
 		// creates a new controller based on the default route
 		$controllers = $app['controllers_factory'];
 
 		$controllers->get('/detail.html', function (Application $app){
-				
-			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/flickr/detail.html';
-			$appFile = $app['spa_files_dir'] . 'flickr/detail.html';
-			
-			if(file_exists($localFile)){
-			
-				return new Response(
-            		file_get_contents($localFile),
-					200
-				);
-				
-			}elseif(file_exists($appFile)){
-					
-				return new Response(
-            		file_get_contents($appFile),
-					200
-				);
-					
+
+			if($app['spa'] === true && $app['search_engine'] === false){
+				return $app['bundle.flickr.detail']();
 			}else{
-				
-				$app->abort(404, "Could not find view file detail.html");
-				
+				return $app['bundle.flickr.full_page.detail']();
 			}
 			
-		});
+		})->bind('flickr-detail');
 
 		$controllers->get('/flickr.html', function (Application $app){
 
-			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/flickr/flickr.html';
-			$appFile = $app['spa_files_dir'] . 'flickr/flickr.html';
-			
-			if(file_exists($localFile)){
-			
-				return new Response(
-            		file_get_contents($localFile),
-					200
-				);
-				
-			}elseif(file_exists($appFile)){
-					
-				return new Response(
-            		file_get_contents($appFile),
-					200
-				);
-					
+			if($app['spa'] === true && $app['search_engine'] === false){
+				return $app['bundle.flickr.index']();
 			}else{
-				
-				$app->abort(404, "Could not find view file flickr.html");
-				
+				return $app['bundle.flickr.full_page.index']();
 			}
 			
-		});
+		})->bind('flickr-index');
 
 		$controllers->get('/flickr.js', function (Application $app){
 		
@@ -99,15 +65,75 @@ class ServiceController extends AbstractServiceController
 	//Service interface
 	public function register(Application $app)
 	{
-        
+
+		$app['bundle.flickr.full_page.index'] = $app->protect(function() use ($app){
+ 			return 'Do full page flickr index';
+ 		});
+ 
+ 		$app['bundle.flickr.index'] = $app->protect(function() use ($app){
+
+			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/flickr/flickr.html';
+			$appFile = $app['spa_files_dir'] . 'flickr/flickr.html';
+			
+			if(file_exists($localFile)){
+			
+				return new Response(
+            		file_get_contents($localFile),
+					200
+				);
+				
+			}elseif(file_exists($appFile)){
+					
+				return new Response(
+            		file_get_contents($appFile),
+					200
+				);
+					
+			}else{
+				
+				$app->abort(404, "Could not find view file flickr.html");
+				
+			}
+			
+		});  
+
+		$app['bundle.flickr.full_page.detail'] = $app->protect(function() use ($app){
+ 			return 'Do full page flickr detail';
+ 		});
+ 
+ 		$app['bundle.flickr.detail'] = $app->protect(function() use ($app){
+
+			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/flickr/detail.html';
+			$appFile = $app['spa_files_dir'] . 'flickr/detail.html';
+			
+			if(file_exists($localFile)){
+			
+				return new Response(
+            		file_get_contents($localFile),
+					200
+				);
+				
+			}elseif(file_exists($appFile)){
+					
+				return new Response(
+            		file_get_contents($appFile),
+					200
+				);
+					
+			}else{
+				
+				$app->abort(404, "Could not find view file detail.html");
+				
+			}
+			
+		});   
+
     }
 
 	//Service interface
     public function boot(Application $app)
     {
 
-		
-    	
     }
 
 }
