242e8c6c7fff326f9ac370c77d51cd451d5a03e5
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-04 21:09:53 +0100

    Added method mapControllers to map controllers to services

--- src/OpenBuild/Abstracts/ServiceController.php
+++ src/OpenBuild/Abstracts/ServiceController.php
@@ -29,6 +29,48 @@ abstract class ServiceController implements ServiceProviderInterface, Controller
 			
 	}
 
+	protected function mapControllers($app, $map){
+	
+		$controllers = $app['controllers_factory'];
+
+		foreach($map AS $key => $fullPage){
+		
+			$split = preg_split('/(\.|-)/', $key);	
+		
+			$module = $split[0];
+			$page = $split[1];
+			$extension = $split[2];
+			
+			$bind = $module . '-' . $page;
+			$uri  = '/'. $page . '.' . $extension;
+			$spa  = 'bundle.' . $module . '.' . $page;
+			$full = 'bundle.' . $module .'.full_page.' . $page;
+
+/*
+			var_dump($bind);
+			var_dump($uri);
+			var_dump($spa);
+			var_dump($full);
+			var_dump($fullPage);
+*/
+			$func = function(Application $app) use ($spa, $full, $fullPage){
+			
+				if($app['spa'] === true && $app['search_engine'] === false){
+					return $app[$spa]();
+				}else{
+					return $app[$full]();
+				}
+			
+			};
+			
+			$controllers->get($uri, $func)->bind($bind);
+		
+		}
+
+		return $controllers;
+	
+	}
+
     /**
      *
      * @param Application $app An Application instance
