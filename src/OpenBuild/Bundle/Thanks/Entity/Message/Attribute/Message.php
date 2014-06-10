da900244db2be96588a0930fe2a75320edbde68f
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-10 20:57:14 +0100

    Added method getHTML, added class Parsedown

--- src/OpenBuild/Bundle/Thanks/Entity/Message/Attribute/Message.php
+++ src/OpenBuild/Bundle/Thanks/Entity/Message/Attribute/Message.php
@@ -2,18 +2,26 @@
 
 namespace OpenBuild\Bundle\Thanks\Entity\Message\Attribute;
 
+use Parsedown;
+
 class Message
 {
     private $value;
+    private $parsedown;
 
     public function __construct($value)
     {
         $this->value = $value;
+        $this->parsedown = new Parsedown();
     }
 
     public function getValue()
     {
         return $this->value;
+    }
+    
+    public function getHTML(){
+    	return $this->parsedown->text($this->value);
     }
     
 }
