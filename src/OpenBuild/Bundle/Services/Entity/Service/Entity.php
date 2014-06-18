aab4e2d2b783e8be139aa6e4c8d16b452266b5c9
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-18 21:25:52 +0100

    Added copyright and license information

--- src/OpenBuild/Bundle/Services/Entity/Service/Entity.php
+++ src/OpenBuild/Bundle/Services/Entity/Service/Entity.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OpenBuild framework https://github.com/openbuild/core
+ *
+ * (c) Danny Lewis <openbuild.sheffield@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OpenBuild\Bundle\Services\Entity\Service;
 
 use OpenBuild\Bundle\Services\Entity\Service\Attribute\Id;
