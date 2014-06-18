32929556311c44894b6fe27a7825e388de894ae2
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-18 21:25:33 +0100

    Added copyright and license information

--- src/OpenBuild/Bundle/Error/Entity/Message/Entity.php
+++ src/OpenBuild/Bundle/Error/Entity/Message/Entity.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OpenBuild framework https://github.com/openbuild/core
+ *
+ * (c) Danny Lewis <openbuild.sheffield@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OpenBuild\Bundle\Error\Entity\Message;
 
 use OpenBuild\Bundle\Error\Entity\Message\Attribute\Code;
