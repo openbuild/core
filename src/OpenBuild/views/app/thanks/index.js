7b7de00a4416e00c7e1eb324ec599a0e483ae933
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-20 20:07:29 +0100

    Updated js view

--- src/OpenBuild/views/app/thanks/index.js
+++ src/OpenBuild/views/app/thanks/index.js
@@ -3,7 +3,17 @@
 	var ctor = {};
 
 	events.includeIn(ctor);
+	
+	ctor.introductionTitle = '{{ introduction.getTitle().getValue() }}';
 
+	ctor.introductionBody = '{{ introduction.getBody().getValue() }}';
+	
+	ctor.messages = [
+		{% for message in messages %}
+		'{{ message.getMessage().getHTML()|escape('js') }}',
+		{% endfor %}
+	];
+	
     return ctor;
     
 });
