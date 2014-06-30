17a23c0c598d8a2810dda8aac74dc1ee7b29c27a
dannylewis <dannylewis.sheffield@googlemail.com>
2014-06-30 01:35:32 +0100

    Moved developer views to developer module and updated service controller

--- src/OpenBuild/Bundle/Developer/Provider/ServiceController.php
+++ src/OpenBuild/Bundle/Developer/Provider/ServiceController.php
@@ -29,6 +29,8 @@ class ServiceController extends AbstractServiceController
 			'developer-index.js' => true
 		));
 
+		$app['twig.loader.filesystem']->addPath(__DIR__.'/../View', 'developer');
+
 		return $controllers;
 
 	}
@@ -38,53 +40,21 @@ class ServiceController extends AbstractServiceController
 	{
         
         $app['bundle.developer.full_page.index'] = $app->protect(function() use ($app){
- 			return 'Do full page contact index';
+ 			return $app->render('@developer/index.full.html', []);
  		});
  
  		$app['bundle.developer.index'] = $app->protect(function() use ($app){
 
-			$localFile = $app['request']->server->get('DOCUMENT_ROOT') . '../views/app/developer/index.html';
-			$appFile = $app['spa_files_dir'] . 'developer/index.html';
-			
-			if(file_exists($localFile)){
-			
-				return new Response(
-            		file_get_contents($localFile),
-					200
-				);
-				
-			}elseif(file_exists($appFile)){
-					
-				return new Response(
-            		file_get_contents($appFile),
-					200
-				);
-					
-			}else{
-				
-				$app->abort(404, "Could not find view file developer/index.html");
-				
-			}
+			return $app->render('@developer/index.html', []);
 
 		});
 		
 		$app['bundle.developer.index.js'] = $app->protect(function() use ($app){
 		
-			$appFile = $app['spa_files_dir'] . 'developer/index.js';
+			$response = new Response();
+			$response->headers->set('content-type', 'application/javascript');
 			
-			if(file_exists($appFile)){
-					
-				return new Response(
-            		file_get_contents($appFile),
-					200,
-					array('content-type' => 'application/javascript')
-				);
-					
-			}else{
-				
-				$app->abort(404, "Could not find view file developer/index.js");
-				
-			}
+			return $app->render('@developer/index.js', [], $response);
 		
 		});
 		
