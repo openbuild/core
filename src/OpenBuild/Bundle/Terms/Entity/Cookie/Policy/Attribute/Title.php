6f834c656cdb3e9786cc48cefa4749f2b7553b9b
dannylewis <dannylewis.sheffield@googlemail.com>
2014-07-02 21:02:43 +0100

    Updated terms entity attributes to use abstract classes

--- src/OpenBuild/Bundle/Terms/Entity/Cookie/Policy/Attribute/Title.php
+++ src/OpenBuild/Bundle/Terms/Entity/Cookie/Policy/Attribute/Title.php
@@ -11,18 +11,11 @@
 
 namespace OpenBuild\Bundle\Terms\Entity\Cookie\Policy\Attribute;
 
-class Title
+class Title extends \OpenBuild\Abstracts\Attribute\Parsedown
 {
-    private $value;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
-    public function getValue()
-    {
-        return $this->value;
-    }
-    
+	
+	public function isValid(){
+	
+	}
+	
 }
