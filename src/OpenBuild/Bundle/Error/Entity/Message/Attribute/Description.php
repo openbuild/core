95795caca4d3dbf2e7442c1cda36484d842a75c7
dannylewis <dannylewis.sheffield@googlemail.com>
2014-07-02 21:07:26 +0100

    Updated error entity attributes to use abstract classes

--- src/OpenBuild/Bundle/Error/Entity/Message/Attribute/Description.php
+++ src/OpenBuild/Bundle/Error/Entity/Message/Attribute/Description.php
@@ -11,26 +11,11 @@
 
 namespace OpenBuild\Bundle\Error\Entity\Message\Attribute;
 
-use Parsedown;
-
-class Description
+class Description extends \OpenBuild\Abstracts\Attribute\Parsedown
 {
-    private $value;
-    private $parsedown;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-        $this->parsedown = new Parsedown();
-    }
-
-    public function getValue()
-    {
-        return $this->value;
-    }
-    
-    public function getHTML(){
-    	return $this->parsedown->text($this->value);
-    }
-    
+	
+	public function isValid(){
+	
+	}
+	
 }
