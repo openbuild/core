3d87f5de87ddc40997c81de1c78e59f82a91eb09
dannylewis <dannylewis.sheffield@googlemail.com>
2014-07-02 20:52:58 +0100

    Updated thanks entity attributes to use abstract classes

--- src/OpenBuild/Bundle/Thanks/Entity/Introduction/Attribute/Body.php
+++ src/OpenBuild/Bundle/Thanks/Entity/Introduction/Attribute/Body.php
@@ -11,18 +11,11 @@
 
 namespace OpenBuild\Bundle\Thanks\Entity\Introduction\Attribute;
 
-class Body
+class Body extends \OpenBuild\Abstracts\Attribute
 {
-    private $value;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
-    public function getValue()
-    {
-        return $this->value;
-    }
-    
+	
+	public function isValid(){
+	
+	}
+	
 }
