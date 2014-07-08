18f8f24eb756fb34db6463303ab907dc001c7dd6
dannylewis <dannylewis.sheffield@googlemail.com>
2014-07-08 20:55:25 +0100

    Start of an event for requesting contact

---
+++ src/OpenBuild/Bundle/Contact/Event/Contact/Name.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the OpenBuild framework https://github.com/openbuild/core
+ *
+ * (c) Danny Lewis <openbuild.sheffield@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OpenBuild\Bundle\Contact\Event\Contact;
+
+final class Name
+{
+
+    const create = 'contact.event.contact.create';
+    
+}
