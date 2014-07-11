6adee1d4f7a1ee98de19c261a63e298f9d5d4b73
dannylewis <dannylewis.sheffield@googlemail.com>
2014-07-11 21:41:36 +0100

    Added contact event

---
+++ src/OpenBuild/Bundle/Contact/Event/Contact/Event/Create.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the OpenBuild framework https://github.com/openbuild/core
+ *
+ * (c) Danny Lewis <openbuild.sheffield@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OpenBuild\Bundle\Contact\Event\Contact\Event;
+
+use Symfony\Component\EventDispatcher\Event AS SymfonyEvent;
+
+class Create extends SymfonyEvent
+{
+
+	protected $data;
+	protected $date;
+
+	public function __construct($data)
+	{
+		$this->data = $data;
+		$this->date = date('Y-m-d h:i:s');
+	}
+	
+	public function getData(){
+		return $this->data;
+	}
+	
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+}
